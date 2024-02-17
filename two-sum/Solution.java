72fd4d0410e437f1acbee28e1c000caab875a301
Muhammad Zohaib <mzohaib.webdoctor@gmail.com>
2024-02-17 13:12:11 +0400

    Feat: two sum problem

---
+++ two-sum/Solution.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution{
+
+    public static void main(String[] args)
+    {
+        // int[] nums = {2,7,11,15};
+        // int target = 9;
+
+
+        // int[] indexes = twoSum(nums, target);
+
+        // for(int index: indexes)
+        // {
+        //     System.out.println(index);
+        // }
+
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        for(int index: result2)
+        {
+            System.out.println(index);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target)
+    {
+
+        HashMap<Integer, Integer> hM =  new HashMap<Integer, Integer>();
+
+        for(int i =0; i< nums.length; i++)
+        {
+            int complement = target-nums[i];
+
+            if(hM.containsKey(complement))
+            {
+                return new int[]{hM.get(complement), i};
+            }else{
+                hM.put(nums[i], i);
+            }
+        }
+
+        return new int[]{};
+    }
+}
